efe27187675f14e8bb9c89161e5b985c22421dc9
Brian <brianhlangford@gmail.com>
2022-06-07 21:41:22 -0700

    changed helper functions and added test set to program

--- NN.py
+++ NN.py
@@ -3,11 +3,18 @@ from operator import ne
 from re import T
 import numpy as np
 import math
+
+from sklearn import preprocessing
 from helperFunctions import *
 import string
 import os
+import random
 from scipy.special import softmax
+import seaborn as sb
+import pandas as pd
+import matplotlib.pyplot as plt
 np.set_printoptions(precision=5)
+from sklearn import preprocessing
 
 
 
@@ -19,11 +26,11 @@ if __name__ == "__main__":  # main function call
     
     testdir = dir[0] + '/test.csv'
     traindir = dir[0] + '/train.csv'
-    eta = [0.0001] # eta for each different eta
+    eta = [0.001] # eta for each different eta
     # Get the data and import into arrays
     getData(testdir,test)
     getData(traindir,train)
-   
+    
     # convert all text to a numerical value
     trainAccuracy = []
     testAccuracy = []
@@ -47,7 +54,7 @@ if __name__ == "__main__":  # main function call
     train = np.delete(train,80,1) # delete the home values out of training set
     #print('train:',train)
     HiddenLayer = np.zeros((numHiddenUnits+1,1)) # hold all my outputs from initial dot product
-    momentum = 0.9 # change during experimemts
+    momentum = 0.8 # change during experimemts
     #  train[i] = (80,1460), hiddenLayer = (1460,20), inputToHiddenWeights = (20,1460)
     #print("train[0] shape: ", train[0].shape)
     inputToHiddenWeights = np.random.default_rng().uniform(low=-0.05, high = 0.05, size = (80,numHiddenUnits))
@@ -56,14 +63,9 @@ if __name__ == "__main__":  # main function call
     DeltaHiddenOutputWeights = np.zeros((5,numHiddenUnits+1))# output-to-hidden layer , using for updating actual hiddenweights
     DeltaHiddenInputWeights = np.zeros((numHiddenUnits,80)) # hidden-to-Input weights, using to update inputToHiddenWeights
     # train[i] => (80,) inputToHiddenWeights => (1460,20)
-    mysum = []
-    for row in range(0,len(train)):
-        sum = np.sum(train[row])
-        mysum.append(sum)
-    print("mysum:")
-    print(mysum)
+   
     epoch = 0
-    while(epoch <10):
+    while(epoch <50):
         print("starting Epoch ",epoch)
         correctOutput = 0
         confusionMatrixTest = np.zeros((5,5),dtype=int)
@@ -83,8 +85,7 @@ if __name__ == "__main__":  # main function call
             for l in range(0,outputLayerSum.size):
                 outputLayerSum[l] = 1/(1 + np.exp(-outputLayerSum[l])) # => (5,1)
             # iterpret the output layer as a classification
-            print("outputLayerSum:")
-            print(outputLayerSum)
+            
             prediction = np.argmax(outputLayerSum)
             #print('Prediction: ', prediction, ' truth[',i,']: ',truth[i])
             if prediction != truth[i]:
@@ -103,7 +104,7 @@ if __name__ == "__main__":  # main function call
                 hiddenToOutputWeights += DeltaHiddenOutputWeights .T #transpose, updating weights
                 inputToHiddenWeights += DeltaHiddenInputWeights.T  #updating weights
             if(truth[i]==prediction):
-                print("a MATCH!!!")
+                
                 correctOutput +=  1
                 confusionRow = prediction
                 confusioncol = truth[i]
@@ -115,6 +116,58 @@ if __name__ == "__main__":  # main function call
         print("trainAccuracy: ")
         print(trainAccuracy)
         epoch += 1
-      
-    print(len(train))
-    print((len(train[0])))
+        
+    #---------------------------------------TEST-------------------------------------#
+testtruth = np.zeros((1461)).astype(int)
+createTruthValues(testtruth,test)
+test = np.array(test)
+test = np.delete(test,0,0) # delete the row header
+test[:,0] = 1 # set id to bias in train
+convertData(test)
+test = test.astype(float)
+correctOutput = 0
+confusionMatrixTest = np.zeros((5,5),dtype=int)
+for i in range(0,len(test)):
+# Hidden Layer before activation 
+    hiddenLayerSum = np.dot(np.transpose(test[i]),inputToHiddenWeights) # hiddenLayerSum (1,20)
+            
+    #squash hidden layer
+    for j in range(0,hiddenLayerSum.size):
+        hiddenLayerSum[j] = 1/(1+ np.exp(-hiddenLayerSum[j])) # => (20,1)
+        HiddenLayer[j+1] = hiddenLayerSum[j] #putting into hiddenLayer
+
+        for k in range(0,hiddenLayerSum.size):
+            outputLayerSum = np.dot(np.transpose(hiddenToOutputWeights),HiddenLayer) # => (5,1)
+            
+        #squash output Layer
+        for l in range(0,outputLayerSum.size):
+            outputLayerSum[l] = 1/(1 + np.exp(-outputLayerSum[l])) # => (5,1)
+        # iterpret the output layer as a classification
+            
+        prediction = np.argmax(outputLayerSum)
+        #print('Prediction: ', prediction, ' truth[',i,']: ',truth[i])
+        if prediction != testtruth[i]:
+            outputError = np.zeros((5,1))
+            hiddenError = np.zeros((numHiddenUnits,1))
+            for m in range(0,len(outputError)):
+                outputError[m] = outputLayerSum[m]*(1-outputLayerSum[m])*(truth[m]-outputLayerSum[m]) #producing outputError
+            for n in range(0,len(hiddenToOutputWeights)-1):
+                sum = np.dot(hiddenToOutputWeights,outputError)
+                sum = np.sum(sum)
+            #hiddenLayer => (21,1), hiddenError = > (21,1)
+            hiddenError[n] = HiddenLayer[n+1]*(1-HiddenLayer[n+1])*sum #deltaOutput
+            if(testtruth[i]==prediction):
+                correctOutput +=  1
+                confusionRow = prediction
+                confusioncol = testtruth[i]
+                confusionMatrixTest[confusionRow][confusioncol] += 1
+    testAccuracy.append(correctOutput/len(test))
+    #---------------------------------------END TEST---------------------------------#
+    plt.plot(trainAccuracy)
+    plt.plot(testAccuracy)
+    plt.title('Training & Test Plot')
+    plt.suptitle('eta = 0.1')
+    plt.ylabel('Accuracy')
+    plt.xlabel('Epochs')
+    plt.legend('trainig','test')
+    plt.show()
